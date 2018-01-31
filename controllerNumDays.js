69ad1d2fb105e45bf1cec64778425dcb5748a10f
Ryz <rjenvey@hotmail.co.uk>
2018-01-31 22:34:50 +0000

    simplified by hijacking location.reload() and finished mvp

--- controllerNumDays.js
+++ controllerNumDays.js
@@ -1,3 +1,4 @@
+// number of days in a month
 const monthDays = [
   {month: 'January', numberOfDays: 31},
   {month: 'February', numberOfDays: 28},
@@ -16,30 +17,34 @@ const monthDays = [
 const getRandom = (myList) => myList[Math.floor(Math.random() * myList.length)]
 const printAtId = (id, s) => document.getElementById(id).innerHTML = s
 
-let numDaysHidden = true
+const randomMonth = getRandom(monthDays)
+printAtId('month', randomMonth.month)
 
-const reset = () => {
-  const randomMonth = getRandom(monthDays)
-  printAtId('month', randomMonth.month)
-  printAtId('numDays', randomMonth.numberOfDays)
-}
 
-const toggleNumDaysVisibility = () => {
-  let element = document.getElementById('numDays')
-  if (numDaysHidden) {
-    element.style.display = 'block'
-    numDaysHidden = false
-  } else {
-    element.style.display = 'none'
-    numDaysHidden = true
-    reset()
-  }
-}
+// weekday of a given date
+const now = new Date().getTime()
+const twentyFourHours = 86400000
+const limitOnDaysForward = 90
+const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-reset()
+let randomFutureDate = new Date(now + (twentyFourHours * Math.random() * limitOnDaysForward))
+printAtId('futureDate', randomFutureDate.toUTCString().substring(5,11))
 
+
+//revealing and reloading on keypress
+let numDaysVisible = false
+let weekDayVisible = false
 document.addEventListener('keydown', (event) => {
-  if (event.keyCode === 32) toggleNumDaysVisibility()
-  location.reload()
+  if (event.keyCode === 32 ){
+    if (numDaysVisible === false) {
+      printAtId('numDays', randomMonth.numberOfDays)
+      numDaysVisible = true
+    } else location.reload()
+  }
+  if (event.keyCode === 68 ){
+    if (weekDayVisible === false) {
+      printAtId('dayOfWeek', daysOfTheWeek[randomFutureDate.getDay()])
+      weekDayVisible = true
+    } else location.reload()
+  }
 })
-
